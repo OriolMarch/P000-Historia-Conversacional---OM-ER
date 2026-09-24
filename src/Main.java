ab102767b1b8a3fb43954b88ccb3e49c698ac7e7
oriolmarch <alu.oriol.march@mataro.epiaedu.cat>
2026-09-24 17:44:21 +0200

    Zones Definides + Array

--- src/Main.java
+++ src/Main.java
@@ -36,6 +36,49 @@ public class Main {
     }
 
     public void crearMon() {
+        Zona dormitoris = new Zona(1,"Dormitoris",
+                "Files de capsules d'hibernacio amb llums blaves. La teva encara es oberta i calenta.");
+        Zona passadis = new Zona (2, "Passadis Central",
+                "Un passadis llarg que travessa tota la nau. Els fluorescents parpellegen.");
+        Zona oficina = new Zona(3, "Oficina Principal", 
+                "Escriptoris plens de papers i informes. Un dels calaixos et sona d'alguna cosa.");
+        Zona comandaments = new Zona (4, "Sala de Comandaments",
+                "Pantalles, botons i una gran placa vermella per engegar motors.");
+        Zona vestuaris = new Zona(5, "Vestuaris",
+                "Armariets oberts i bancs. Hi ha un vestit espacial penjat d'un ganxo.");
+        Zona infermeria = new Zona(6, "Infermeria",
+                "Lliteres fetes i una farmaciola buida. Fa una olor estranya, com de peix.");
+        Zona taller = new Zona(7, "Taller",
+                "El taller de manteniment, ple d'eines ben endrecades.");
+        Zona cuina = new Zona(8, "Cuina",
+                "Una cuina petita amb olor de cafe cremat.");
+        Zona magatzem = new Zona(9, "Magatzem",
+                "Caixes apilades fins al sostre amb material de recanvi.");
+        Zona esclusa = new Zona(10, "Esclusa",
+                "La porta exterior de la nau. Al darrere nomes hi ha el buit.");
+        Zona motors = new Zona(11, "Zona de Motors",
+                "Ets fora de la nau, enganxat al casc. Els propulsors fumegen.");
+    
+    zones = new Zona[11];
+    zones[0] = dormitoris;
+    zones[1] = passadis;
+    zones[2] = oficina;
+    zones[3] = comandaments;
+    zones[4] = vestuaris;
+    zones[5] = infermeria;
+    zones[6] = taller;
+    zones[7] = cuina;
+    zones[8] = magatzem;
+    zones[9] = esclusa;
+    zones[10] = motors;
+
+    
+
+
+
+        
+
+        
     
     }
 
